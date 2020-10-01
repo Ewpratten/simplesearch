17b06bc71c2462c57a714f1aae16880def1aad39
Evan Pratten <ewpratten@gmail.com>
2020-10-01 14:15:14 -0400

    Perpare for server work

--- database/database.py
+++ database/database.py
@@ -9,7 +9,17 @@ _engine_db_path = "./engine_database.json"
 # Search tools
 _page = Query()
 
-def isPageIndexed(url: str, db: TinyDB = None):
+def isPageIndexed(url: str, db: TinyDB = None) -> bool:
+    """Check if a url has been indexed
+
+    Args:
+        url (str): URL to check
+        db (TinyDB, optional): This is only used internally. Defaults to None.
+
+    Returns:
+        bool: Has been indexed?
+    """
+    
     # Handle external call
     if db == None:
         with TinyDB(_engine_db_path) as db:
@@ -18,6 +28,15 @@ def isPageIndexed(url: str, db: TinyDB = None):
         return len(db.search(_page.url == url)) > 0
 
 def indexNewWebpage(title: str, url: str, contents: List[str], timestamp: int) -> None:
+    """Index a new webpage
+
+    Args:
+        title (str): Page title
+        url (str): Page URL
+        contents (List[str]): Page contents
+        timestamp (int): Timestamp of the index
+    """
+
     with TinyDB(_engine_db_path) as db:
         # If an index exists for this page, update it
         if isPageIndexed(url, db=db):
@@ -36,4 +55,13 @@ def indexNewWebpage(title: str, url: str, contents: List[str], timestamp: int) -
             })
 
 def query(keywords: List[str]) -> List[dict]:
+    """Query for a page
+
+    Args:
+        keywords (List[str]): Search keywords
+
+    Returns:
+        List[dict]: All matching pages
+    """ 
+    
     return []
---
+++ server/main.py
@@ -0,0 +1,3 @@
+# Web UI
+
+import flask
